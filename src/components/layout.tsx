844837835c134a929bb8f66c5f3663da77ba0de2
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-13 09:54:08 +0200

    b

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,22 +3,26 @@ import Footer from './footer'
 import Meta from './meta'
 import Seo from './seo'
 import { AppProvider } from "./context/AppContext";
+import { ApolloProvider } from "@apollo/client";
+import client from "./ApolloClient";
+
 
 export default function Layout({ preview, children, footerMenuItems, data }) {
   const seo = data?.seo
-	const uri = data?.uri
+  const uri = data?.uri
   return (
     <>
-    <AppProvider>
-      <Meta />
-			<Seo seo={seo} uri={uri}/>
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer menuItems={footerMenuItems}/>
-    </AppProvider>
-
+      <AppProvider>
+        <ApolloProvider client={client}>
+          <Meta />
+          <Seo seo={seo} uri={uri} />
+          <div className="min-h-screen">
+            <Alert preview={preview} />
+            <main>{children}</main>
+          </div>
+          <Footer menuItems={footerMenuItems} />
+        </ApolloProvider>
+      </AppProvider>
     </>
   )
 }
