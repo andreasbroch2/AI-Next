b2af8425f2055bfc4585bc7151203409918f9daa
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-14 10:38:04 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,17 +22,12 @@ const poppins = localFont({
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-P8GKKRH' });
+}, []);
   return (
     <>
     <Head>
-    <Script id="google-tag-manager" strategy='afterInteractive'>
-          {`
-          (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TG6SCN8');
-          `}
-        </Script>
     </Head>
     <div className={poppins.className}>
       <Script src="/js/app.js" />
