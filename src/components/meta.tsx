de36166eb6a40dec7457e1989a111a1d76457cc0
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-17 10:36:37 +0200

    Update meta.tsx

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -8,6 +8,7 @@ export default function Meta() {
         <link rel="shortcut icon" href="/favicon/favicon.png" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="msapplication-config" content="/favicon/favicon.png" />
+        <meta name="facebook-domain-verification" content="o2k2qwp36ooqmeihp3w4pi1a740q3y" />
         <meta name="theme-color" content="#000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
